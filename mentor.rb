1a93bab507e9f5e497e49ab1786df1bd4fae9016
yuuki.tamura <fxxk.the.rock@gmail.com>
2022-03-29 14:52:50 +0000

    add mentor.rb

--- mentor.rb
+++ mentor.rb
@@ -1,29 +1,25 @@
 class Mentor
-  attr_accessor :name
   
   def initialize(name)
-    self.name = name
+    @name = name
   end
+  
   def job
-    puts"#{self.name}です。私は現役のITプロフェッショナルです。"
+    puts"#{@name}です。私は現役のITプロフェッショナルです。"
   end
 end
 
 class RailsMentor < Mentor
-  @@type = '赤出'
-  
-  attr_accessor :suffix
-  
+ 
   def initialize(suffix)
-    super('赤出')
-    self.suffix = suffix
+    @suffix = suffix
   end
+  
   def job
-    @@type + self.suffix
-    puts @@type + "です。私はRubyとRailsでWebアプリケーションを作ります。"
+    puts"#{@suffix}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
 end
 kirameki = Mentor.new('煌木')
-kirameki.job
 akaide = RailsMentor.new('赤出')
+kirameki.job
 akaide.job
